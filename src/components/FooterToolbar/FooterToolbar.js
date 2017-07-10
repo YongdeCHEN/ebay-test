9e53bc984f50a70dfacd4783c9be2a7d826c627c
Peter CHEN <yongdechen@gmail.com>
2017-07-11 09:14:40 +1000

    add some comment

--- src/components/FooterToolbar/FooterToolbar.js
+++ src/components/FooterToolbar/FooterToolbar.js
@@ -3,6 +3,19 @@ import './FooterToolbar.css';
 import leftImg from '../../assets/left.png';
 import rightImg from '../../assets/right.png';
 
+/**
+ * Component name: FooterToolbar
+ * Input props:
+ *      leftTitle: The title of left Nav on the Footer Toolbar
+ *      rightTitle: The title of right Nav on the Footer Toolbar
+ *      onClickPrev: The function when the "left Nav" was clicked
+ *      onClickNext: The function when the "right Nav" was clicked
+ * Functions: Display the Footer Toolbar. There are two nav functions in the toolbar.
+ *   when the left title was clicked, the content of page above will be changed accordingly.
+ * Changed History:
+ *    Peter Chen       11 Jul 2017     initial version 
+ */
+
 class FooterToolbar extends Component {
   render() {
     const {leftTitle, rightTitle, onClickPrev, onClickNext} = this.props;
